3cc82b665767ff9b5f88caae491e17430b0e61cd
Miles <Miles@DESKTOP-CFVC3SV.mshome.net>
2021-01-10 19:09:08 -0700

    Commit Test

--- src/main/java/com/t07m/encryption/Encryption.java
+++ src/main/java/com/t07m/encryption/Encryption.java
@@ -33,6 +33,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class Encryption {
 	
+	
 	public static SecretKeySpec createSecretKey(String password, String salt) {
 		try {
 			return createSecretKey(password.toCharArray(), salt.getBytes(), 40000, 128);
